d9e7786ded2ccb134315117d7ea9250238a79beb
td <songxudong@promni.cn>
2022-11-14 15:50:13 +0800

    feat:env

--- src/PromniLibPublicCacheServiceProvider.php
+++ src/PromniLibPublicCacheServiceProvider.php
@@ -17,32 +17,41 @@ class PromniLibPublicCacheServiceProvider extends ServiceProvider
                 __DIR__ . '/../config/promni-common-cache.php' => config_path('promni-common-cache.php'),
             ]
         );
+    }
 
+    public function register()
+    {
         /** @var Repository $config */
         $config = $this->app->get('config');
+        $commonCacheConfig = $this->app->make('config')->get('promni-common-cache');
+
+        /** redis配置 */
+        $redisConfig = $commonCacheConfig['redis'] ?? [];
         $config->set(
             'database.redis.gm_public',
             [
-                'host'     => config('promni-common-cache.redis.host'),
-                'port'     => config('promni-common-cache.redis.port'),
-                'database' => config('promni-common-cache.redis.database'),
-                'password' => config('promni-common-cache.redis.password'),
+                'host'     => $redisConfig['host'],
+                'port'     => $redisConfig['port'],
+                'database' => $redisConfig['database'],
+                'password' => $redisConfig['password'],
                 'options'  => [
-                    'prefix' => config('promni-common-cache.redis.prefix'),
+                    'prefix' => $redisConfig['prefix'],
                 ],
             ]
         );
 
+        /** mysql数据库配置 */
+        $mysqlCrmConfig = $commonCacheConfig['mysql-crm'] ?? [];
         $config->set(
             'database.connections.crm_public',
             [
                 'driver'         => 'mysql',
-                'host'           => config('promni-common-cache.mysql-crm.host'),
-                'port'           => config('promni-common-cache.mysql-crm.port'),
-                'database'       => config('promni-common-cache.mysql-crm.database'),
-                'username'       => config('promni-common-cache.mysql-crm.username'),
-                'password'       => config('promni-common-cache.mysql-crm.password'),
-                'unix_socket'    => config('promni-common-cache.mysql-crm.unix_socket'),
+                'host'           => $mysqlCrmConfig['host'],
+                'port'           => $mysqlCrmConfig['port'],
+                'database'       => $mysqlCrmConfig['database'],
+                'username'       => $mysqlCrmConfig['username'],
+                'password'       => $mysqlCrmConfig['password'],
+                'unix_socket'    => $mysqlCrmConfig['unix_socket'],
                 'charset'        => 'utf8mb4',
                 'collation'      => 'utf8mb4_unicode_ci',
                 'prefix'         => '',
@@ -51,14 +60,10 @@ class PromniLibPublicCacheServiceProvider extends ServiceProvider
                 'engine'         => null,
                 'options'        => extension_loaded('pdo_mysql') ? array_filter(
                     [
-                        PDO::MYSQL_ATTR_SSL_CA => config('promni-common-cache.mysql-crm.mysql_attr_ssl_ca'),
+                        PDO::MYSQL_ATTR_SSL_CA => $mysqlCrmConfig['mysql_attr_ssl_ca'],
                     ]
                 ) : [],
             ]
         );
-    }
-
-    public function register()
-    {
     }
 }
